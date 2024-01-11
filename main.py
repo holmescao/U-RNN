298ea19dbcddfc8f34e32027b94d49c48decc5a7
holmescao <holmescaoxy@gmail.com>
2024-01-12 00:37:53 +0800

    20240107_223311_284164
        输入:
        - 将输入区域为500x500的网格; 16个样本，train:test=12:4
        - 所有变量都用最大最小归一化；DEM_max=最值, 降雨_max=峰值，内涝_max=峰值3500mm，不透水性_max=0.95，排水口_max=1
        - 去除机理:flow_volumn
        - loc=0; window_size改为事件长度; seq_num=45, 顺序窗口
        - 将完整的一场降雨-内涝事件送进去训练（包括降雨期间和降雨结束的退水过程）
    
        输出: 当前时刻的洪水
        模型: GRU, 卷积核1x1；stride=2改为1，用平均池化来做下采样;(16_64_96);
            cls和reg走branch+4层;
            Conv的head:cls不加GN,reg不加GN;ED不加GN;
            用cls修正reg
        损失函数: 新的FocalBCE_and_WMSE@0.8; 不上限1e+5; reg*2*4.5;
        head激活函数：acts = ["silu"] * 3 + ["sigmoid", "lrelu"];
        模型优化方案:
        - lr=1e-2(multi), patience=10, factor=0.9, min_lr=1e-4; epoch=100; schedule: WarmUpCosineAnneal；if True:
        - 用checkpoint技术; 去除AMP技术(pytorch版本，不做梯度scale)
        其他: 每个epoch都保存；DDP技术

--- main.py
+++ main.py
@@ -106,6 +106,7 @@ def WarmUpCosineAnneal(optimizer, warm_up_iter, T_max, lr_max, lr_min):
     # 设置学习率调整规则 - Warm up + Cosine Anneal
     def lambda0(cur_iter):
         # print("########################cur_iter:", cur_iter)
+        cur_iter = 414
         lr = (
             cur_iter / warm_up_iter
             if cur_iter < warm_up_iter
@@ -122,7 +123,7 @@ def WarmUpCosineAnneal(optimizer, warm_up_iter, T_max, lr_max, lr_min):
             )
             / 0.1
         )
-
+        print(f"学习率:{lr}")
         return lr
 
     # LambdaLR
